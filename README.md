82aabaec88a48803bb42ca9e8c9de7eb6df6805f
Paul T <paultavares1@gmail.com>
2012-10-27 18:16:02 -0400

    Code clean up and a working version of sticking element on both window object and fixed height element. Edited README file.

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ jquery.stickOnScroll
 
 **SEE PROJECT WEB SITE: http://purtuga.github.com/jquery.stickOnScroll/**
 
-A jQuery plugin for making elements fixed on the top of the page when scrolling reaches the desired top offset from the viewport (window). The plugin will also scroll that element back up if a footer element is defined. The footer element is watched to see when it comes close to the bottom edge of the fixed element (an offset is provided).
+A jQuery plugin for making elements fixed on the top of the page when scrolling reaches the desired top offset from the viewport (window or fixed height element with overflow=auto|scroll). The plugin will also scroll that element back up if a footer element is defined. The footer element is watched to see when it comes close to the bottom edge of the fixed element (an offset is provided).
+
 
 Usage
 -----
@@ -36,12 +37,16 @@ And the following javascript code:
 
 Will make the h2 heading "Section 1" stick to the top of the page (css position: fixed) and will watch for when the footer element (the paragraph with id = section1footer) comes within 20 pixels of the bottom of the h2 element at which point, the header is scrolled up with the normal flow of the page.
 
-Note that in the example above I set the height of the div.section-title-container. This was just for demonstrations purposes and could have been achieved via other methods, The reason for setting the height of the element around the header is that once the header is fixed on the page, it is removed from the normal flow of the document. By setting the surrounding element's height, the document structure should be maintained when the header is moved to position: position.
+In the example above the height of the div.section-title-container is set prior to calling stickOnScroll(). This was just for demonstrations purposes and could have been achieved via other methods (ie. css). The reason for setting the height of the element around the header is that once the header is fixed on the page, it is removed from the normal flow of the document. By setting the surrounding element's height, the document structure should be maintained when the header is moved to position: position.
 
 This plugin will manipulate the following css properties on the element that will be fixed:
 
 -   top
 -   position
+
+
+_**Note: Internet Explorer Behaviour** When used on a viewport that is not the Window object, the fixed element will be animated (gradual slide down) in Internet Explorer in order to avoid the flashing/flickering affect._
+
 
 
 Options
@@ -50,10 +55,13 @@ Options
 
 -   **topOffset**       :   *Integer. Optional. Default=0* <br />
                             The number of pixels from the top of the viewport (window) before the element triggers to fixed. **Note:** This value must be an integer. 
+
 -   **bottomOffset**    :   *Integer. Optional. Default=0* <br />
                             The number of pixels between the bottom of the fixed element and the top of the footer before the fixed element scrolls back up the page.
+
 -   **footerElement**   :   *HTMLElement|jQuery|selector. Optional. Default=null* <br />
                             The footer element to be used for triggering the fixed element to scroll back up.
+
 -   **stickClass**      :   *String. Optional. Default="stickOnScroll-on" * <br />
                             The class name that will be added to the fixed element when it is fixed on the page.
 
--- src/jquery.stickOnScroll.js
+++ src/jquery.stickOnScroll.js
@@ -9,8 +9,6 @@
  */
 ;(function($){
     
-    
-    
     // Check if we're working in IE. Will control the animation
     // on the fixed elements.
     var isIE = ($.support.optSelected === false ? true : false);
@@ -37,164 +35,113 @@
         for(var i=0,j=elements.length; i<j; i++){
             var o = elements[i];
             
-            // FIXME: What if element had been removed from DOM? Need to remove reference to it.
+            // FIXME: Should the clean up of reference to removed element store the position in the array and delete it later?
             
-            // Get the scroll top position on the view port
-            // and set the maxTop before we stick the element
-            // to be it's "normal" topPosition minus offset
-            var scrollTop   = o.viewport.scrollTop(),
-                maxTop      = (o.eleTop - o.topOffset);
-          
-            // TODO: What about calculating top values with margin's set?
-            // pt.params.footer.css('marginTop').replace(/auto/, 0)
-            
-            // If not using the window object, then stop any IE animation
-            if (o.isWindow === false && isIE) {
-                o.ele.stop();
-            }
-            
-            
-// if (o.ele.data("stickonscroll") === "section7") {
-    // console.log("SECTION7: scrollTop+o.TopOffset[" + (scrollTop + o.topOffset) + "] > maxTop[" + maxTop + "]?");
-// }
-
-
-            // If the current scrollTop position plus the topOffset is greater
-            // than our maxTop value, then make element stick on the page.
-            // if ((scrollTop + o.topOffset) > maxTop) {
-            if (    (o.isWindow === true && (scrollTop + o.topOffset) > maxTop)
-                ||  (o.isWindow === false && o.eleTop < scrollTop )
-            ){
-                
-                
-
-
-
-                
-                
-                // TODO: clean up
-                //o.eleStickTop = o.ele.position().top;
-                
-                var cssPosition = {
-                        position:   "fixed",
-                        top:        o.topOffset
-                    };
-                if (o.isWindow === false) {
-                    cssPosition = {
-                        position:   "absolute",
-                        top:        (scrollTop + o.topOffset)
-                    };
+            // If element has no parent, then it must have been removed from DOM...
+            // Remove reference to it
+            if (o !== null) {
+                if (o.ele[0].parentNode == null) {
+                    elements[i] = o = null;
                 }
+            }
+            if (o !== null) {
                 
-if (o.ele.data("stickonscroll") === "section1") {
-    console.log("SECTION1: Stick Striggered? Setting top to: " + cssPosition.top);
-}
-                // TODO: clean up
-                // o.ele.css({
-                        // position: "fixed",
-                        // top: o.topOffset
-                    // })
-                    // .addClass(o.stickClass);
+                // Get the scroll top position on the view port
+                // and set the maxTop before we stick the element
+                // to be it's "normal" topPosition minus offset
+                var scrollTop   = o.viewport.scrollTop(),
+                    maxTop      = (o.eleTop - o.topOffset);
+              
+                // TODO: What about calculating top values with margin's set?
+                // pt.params.footer.css('marginTop').replace(/auto/, 0)
                 
-                //o.ele.css(cssPosition).addClass(o.stickClass);
-                
-                o.isStick = true;
-                
-                
-                // ---> HAS FOOTER ELEMENT?
-                // check to see if it we're reaching the footer element,
-                // and if so, scroll the item up with the page
-                if  (o.footerElement.length) {
+                // If not using the window object, then stop any IE animation
+                if (o.isWindow === false && isIE) {
+                    o.ele.stop();
+                }
+    
+                // If the current scrollTop position plus the topOffset is greater
+                // than our maxTop value, then make element stick on the page.
+                // if ((scrollTop + o.topOffset) > maxTop) {
+                if (    (o.isWindow === true && (scrollTop + o.topOffset) > maxTop)
+                    ||  (o.isWindow === false && o.eleTop < scrollTop )
+                ){
                     
-                    // Calculate the distance from the *bottom* of the fixed
-                    // element to the footer element, taking into consideration
-                    // the bottomOffset that may have been set by the user. 
-                    var footerTop   = o.footerElement.position().top,
-                        eleHeight   = o.ele.outerHeight(),
-                        yAxis       = (
-                            cssPosition.top + eleHeight + o.bottomOffset + o.topOffset );
-                        
-                        // yAxis = (o.ele.position().top + eleHeight + o.bottomOffset);
-                                // FIXME: MISSING adding o.topOffset? it was already used in setting cssPosition.top
-               
+                    var cssPosition = {
+                            position:   "fixed",
+                            top:        o.topOffset
+                        };
                     if (o.isWindow === false) {
-                        yAxis = (eleHeight + o.bottomOffset + o.topOffset);
-                    
-                    }else {
-                        yAxis = (o.ele.offset().top + eleHeight + o.bottomOffset);
-                        footerTop = o.footerElement.offset().top;
+                        cssPosition = {
+                            position:   "absolute",
+                            top:        (scrollTop + o.topOffset)
+                        };
                     }
                     
+                    o.isStick = true;
                     
                     
-               
-               
-// if (o.ele.data("stickonscroll") === "section6") {
-    // console.log("SECTION6: yAxis[" + yAxis + "] > footerTop[" + footerTop +"]?");
-// }
-               
-                    if (yAxis > footerTop) {
-                        if (o.isWindow === true) {
-                            cssPosition.top = (  
-                                    footerTop - ( scrollTop + eleHeight + o.bottomOffset )
-                                );
+                    // ---> HAS FOOTER ELEMENT?
+                    // check to see if it we're reaching the footer element,
+                    // and if so, scroll the item up with the page
+                    if  (o.footerElement.length) {
+                        
+                        // Calculate the distance from the *bottom* of the fixed
+                        // element to the footer element, taking into consideration
+                        // the bottomOffset that may have been set by the user. 
+                        var footerTop   = o.footerElement.position().top,
+                            eleHeight   = o.ele.outerHeight(),
+                            yAxis       = (
+                                cssPosition.top + eleHeight + o.bottomOffset + o.topOffset );
                             
-                        // Absolute positioned element
+                        if (o.isWindow === false) {
+                            yAxis = (eleHeight + o.bottomOffset + o.topOffset);
+                        
                         } else {
-                            cssPosition.top = (scrollTop - (yAxis - footerTop));
+                            yAxis = (o.ele.offset().top + eleHeight + o.bottomOffset);
+                            footerTop = o.footerElement.offset().top;
                         }
-                     
-// if (o.ele.data("stickonscroll") === "section7") {
-    // console.log("SECTION7: After footer calculation, new TOP: " + o.topOffset);
-// }
                         
-               
-// if (o.ele.data("stickonscroll") === "section2") {
-    // console.log("Adjusting TOP... yAxis[" + yAxis + "] gt footerTop[" + footerTop + 
-        // "]... isWindow[" + o.isWindow + "]\n   NEW TOP:" + cssPosition.top);
-//     
-// }
+                        if (yAxis > footerTop) {
+                            if (o.isWindow === true) {
+                                cssPosition.top = (  
+                                        footerTop - ( scrollTop + eleHeight + o.bottomOffset )
+                                    );
+                                
+                            // Absolute positioned element
+                            } else {
+                                cssPosition.top = (scrollTop - (yAxis - footerTop));
+                            }
+                        }
+                    }
+                    
+                    // Stick the element
+                    if (isIE && o.isWindow === false) {
+                        o.ele.addClass(o.stickClass)
+                            .css("position", cssPosition.position)
+                            .animate({top: cssPosition.top}, 150);
                         
-                    // TODO: clean up
-                    // } else {
-                        // o.ele.css("top", o.topOffset);
-                    // } else if (o.isWindow === true) {
-                        // cssPosition.top = o.topOffset;
+                    } else {
+                        o.ele.css(cssPosition).addClass(o.stickClass);
+                    }
+                    
+                    
+                // ELSE, If the scrollTop of the view port plus the topOffset is
+                // less than the maxTop, then throw the element back into the 
+                // page normal flow                    
+                } else if ((scrollTop + o.topOffset) <= maxTop) {
+                    if (o.isStick) {
+                        o.ele.css({
+                                position: "",
+                                top: ""
+                            })
+                            .removeClass(o.stickClass);
+                        o.isStick = false;
                     }
                 }
                 
-// if (o.ele.data("stickonscroll") === "section6") {
-    // console.log("SECTION6: setting TOP to:[" + cssPosition.top + "]");
-// }
-                // Stick the element
-                if (isIE && o.isWindow === false) {
-                    o.ele.addClass(o.stickClass)
-                        .css("position", cssPosition.position)
-                        .animate({top: cssPosition.top}, 150);
-                    
-                } else {
-                    o.ele.css(cssPosition).addClass(o.stickClass);
-                }
-                
-                
-            // ELSE, If the scrollTop of the view port plus the topOffset is
-            // less than the maxTop, then throw the element back into the 
-            // page normal flow                    
-            } else if ((scrollTop + o.topOffset) <= maxTop) {
-                if (o.isStick) {
-//                
-// if (o.ele.data("stickonscroll") === "section2") {
-    // console.log("removing stickness....");
-// }
-               
-                    o.ele.css({
-                            position: "",
-                            top: ""
-                        })
-                        .removeClass(o.stickClass);
-                    o.isStick = false;
-                }
-            }
+            }// is element setup null?
+            
         };//end: for()
         
         return this;
